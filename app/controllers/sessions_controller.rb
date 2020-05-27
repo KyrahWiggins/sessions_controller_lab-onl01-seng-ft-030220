4ccad312d79050ac80b0d91b7c7c289a42947859
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-05-27 17:20:49 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,15 @@ class SessionsController < ApplicationController
 
   def create
     if params[:name].nil? || params[:name].empty?
-      redirect_to :login 
-    else 
+      redirect_to :login
+    else
       session[:name] = params[:name]
       redirect_to '/'
-    end 
-  end 
+    end
+  end
 
   def destroy
     session.delete :name
-  end 
+  end
 
 end
